3b43a134d24071a8faf0846d48581e35e54447c1
leonardo <leonardoxrrp@gmail.com>
2022-09-25 19:28:57 +0200

    setup backward navigation for episodes

--- src/components/podcastDetails/PodcastDetails.js
+++ src/components/podcastDetails/PodcastDetails.js
@@ -1,27 +1,32 @@
 import Linkify from 'linkify-react';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import useGetDetails from '../../hooks/useGetDetails';
 import useGetPodcasts from '../../hooks/useGetPodcasts';
 
 function PodcastDetails({ children }) {
+  const navigate = useNavigate();
   const { podcastDetails } = useGetDetails();
   const { podcasts } = useGetPodcasts();
   const { podcastId } = useParams();
+  const handleNavigation = () => {
+    if (window.location.pathname.includes('episode')) return navigate(-1);
+    return navigate(0);
+  };
   const info = podcastDetails.find((detail) => detail.wrapperType === 'track');
   const description = podcasts?.entry?.find((podcast) => podcast.id.attributes['im:id'] === podcastId);
   return (
     <div className="podcastid-container">
       <div>
         <div className="podcast-left-side">
-          <img src={info?.artworkUrl600} alt={info?.collectionName} className="podcast-left-side-image" />
+          <img onClick={handleNavigation} src={info?.artworkUrl600} alt={info?.collectionName} className="podcast-left-side-image podcast-navigation" />
           <div className="podcast-left-side-details">
             <div>
               <hr />
-              <p className="fw-bold">{info?.collectionName}</p>
+              <p onClick={handleNavigation} className="fw-bold podcast-navigation">{info?.collectionName}</p>
               <p>
                 by
-                <span>
+                <span onClick={handleNavigation} className="podcast-navigation">
                   {' '}
                   {info?.collectionName}
                 </span>
