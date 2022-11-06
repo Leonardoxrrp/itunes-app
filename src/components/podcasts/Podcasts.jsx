622c94fc8d1e363e29204a6378aaf0bbde92e0b7
leonardo rojas <leonardoxrrp@gmail.com>
2022-11-07 00:14:12 +0100

    add cors check on homepage

--- src/components/podcasts/Podcasts.jsx
+++ src/components/podcasts/Podcasts.jsx
@@ -1,14 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../../context/AppContext';
 
 import './podcasts.css';
 
 function Podcasts() {
-  const { setPointer, searchResults } = useContext(Context);
+  const { setPointer, searchResults, podcasts } = useContext(Context);
   const textUpperCase = (text) => text.toUpperCase();
+
   return (
-    searchResults.map((podcast) => (
+    <>
+    {podcasts.length === 0 && <p>Please activate CORS <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank">here</a> and refresh the page in order to consume the iTunes API</p>}
+    {searchResults.map((podcast) => (
       <Link onClick={() => setPointer(true)} to={`podcast/${podcast.id.attributes['im:id']}`} style={{ textDecoration: 'none' }} key={podcast['im:name'].label}>
         <div className="podcasts-card">
           <img src={podcast['im:image'][2].label} alt={podcast['im:name'].label} />
@@ -21,7 +24,8 @@ function Podcasts() {
           </div>
         </div>
       </Link>
-    ))
+    ))}
+    </>
   );
 }
 
