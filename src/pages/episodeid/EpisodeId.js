6e24ed7561d7798512bcfbb0e82b33effec0d1e8
leonardo rojas <leonardoxrrp@gmail.com>
2022-11-09 19:53:54 +0100

    add spinner and run lint

--- src/pages/episodeid/EpisodeId.js
+++ src/pages/episodeid/EpisodeId.js
@@ -1,6 +1,7 @@
 import Linkify from 'linkify-react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 import PodcastDetails from '../../components/podcastDetails/PodcastDetails';
 import useGetDetails from '../../hooks/useGetDetails';
 
@@ -9,30 +10,34 @@ function EpisodeId() {
   const { episodeId } = useParams();
   const episode = podcastDetails?.find((element) => Number(element.trackId) === Number(episodeId));
   const description = episode?.shortDescription || episode?.description;
+
   return (
-    <PodcastDetails>
-      <div className="podcastid-right-side">
-        <div className="podcastid-right-side-details">
-          <h2>{episode?.trackName}</h2>
-          <Linkify>
-            <p className="fst-italic">{description}</p>
-          </Linkify>
-          <p />
-          <hr />
-          <audio
-            style={{ width: '100%', marginBottom: '20px' }}
-            controls
-            src={episode?.episodeUrl}
-          >
-            <track
-              default
-              kind="captions"
-              srcLang="en"
-            />
-          </audio>
+    <>
+      {!episode && <Spinner animation="grow" role="status" />}
+      <PodcastDetails>
+        <div className="podcastid-right-side">
+          <div className="podcastid-right-side-details">
+            <h2>{episode?.trackName}</h2>
+            <Linkify>
+              <p className="fst-italic">{description}</p>
+            </Linkify>
+            <p />
+            <hr />
+            <audio
+              style={{ width: '100%', marginBottom: '20px' }}
+              controls
+              src={episode?.episodeUrl}
+            >
+              <track
+                default
+                kind="captions"
+                srcLang="en"
+              />
+            </audio>
+          </div>
         </div>
-      </div>
-    </PodcastDetails>
+      </PodcastDetails>
+    </>
   );
 }
 
--- src/pages/podcastid/PodcastId.js
+++ src/pages/podcastid/PodcastId.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 import EpisodesTable from '../../components/episodes-table/EpisodesTable';
 import PodcastDetails from '../../components/podcastDetails/PodcastDetails';
 import useGetDetails from '../../hooks/useGetDetails';
@@ -6,22 +7,23 @@ import './podcastid.css';
 
 function PodcastId() {
   const { podcastDetails } = useGetDetails();
-
   return (
-    <PodcastDetails>
-      <div className="podcastid-right-side">
-        <div className="podcastid-right-side-episodes">
-          <p className="fw-bold podcastid-episodes-text">
-            <span>Episodes: </span>
-            {podcastDetails.length}
-          </p>
+    <>
+      {podcastDetails.length === 0 && <Spinner animation="grow" role="status" />}
+      <PodcastDetails>
+        <div className="podcastid-right-side">
+          <div className="podcastid-right-side-episodes">
+            <p className="fw-bold podcastid-episodes-text">
+              <span>Episodes: </span>
+              {podcastDetails.length}
+            </p>
+          </div>
+          <div className="podcastid-right-side-details">
+            <EpisodesTable episodes={podcastDetails} />
+          </div>
         </div>
-        <div className="podcastid-right-side-details">
-          <EpisodesTable episodes={podcastDetails} />
-        </div>
-      </div>
-    </PodcastDetails>
-
+      </PodcastDetails>
+    </>
   );
 }
 
